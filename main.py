15fb360a90b4d26669a60726bd3e80241d2b34ef
Daveed <daveed925@gmail.com>
2022-09-12 02:29:01 +0530

    Added a new feature

--- main.py
+++ main.py
@@ -7,13 +7,30 @@ if play == "q":
     quit()
 
 if play == "left":
-    play = input("You've come to a river, would you love to walk around or swim? (Type walk/swim): ").lower()
+    play = input("You've come to a river, will you love to walk around or swim? (Type walk/swim): ").lower()
 
     if play == "walk":
-        print(name, "You don waka go where you no know o. You lost the game!")
+        print(name, "You walked several miles and now out of water. You lost the game!")
     elif answer == "swim":
-        print(name, "You don swim go where you no know sha. You lost the game!")
+        print(name, "You swam across the river and got eaten by a crocodile. You lost the game!")
     else:
         print("Not a valid option")
 elif play == "right":
-    print()      
+    play = input("You've come to an island. Will you call for help or live there forever? (Type call/stay): ").lower()
+
+    if play == "call":
+        print("Call dialing...")
+        play = input("You called and a stranger picked it. Will you reply or ignore? (Type reply/ignore): ").lower()
+
+        if play == "ignore":
+            print("You ignored the stranger, and he got angry. You lost the game!")
+        elif play == "reply":
+            print("You replied the stranger, and you got saved. You won the game!")
+        else:
+            print("Not a valid response")
+    elif play == "stay":
+        print()
+    else:
+        print("Not a valid option")
+else:
+    print("Please enter a valid option")
